490c9ef5f0fedd741af29685612dadbcb5899bb5
Hayden Carlson <hcarlson.0108@gmail.com>
2026-09-14 23:31:30 -0500

    added styles, examples, logic, and a footer

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const checkBtn = document.getElementById('checkBtn');
+const clearBtn = document.getElementById('clearBtn');
 const wordInput = document.getElementById('wordInput');
 const message = document.getElementById('message');
-let userInput = '';
 
+const getPalindrome = (word) => {
+  const arrLength = word.length;
+  for (let i = 0; i < arrLength / 2; i++) {
+    if (word[i] !== word[arrLength - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//get user input
 checkBtn.addEventListener('click', () => {
-  userInput = wordInput.value;
-  console.log(userInput);
+  const userInput = wordInput.value;
+  //turn user input into an array
+  const word = userInput.split('');
+  const isPalindrome = getPalindrome(word);
+  message.textContent = isPalindrome
+    ? `"${userInput}" is a palindrome!`
+    : `"${userInput}" is not a palindrome.`;
+  checkBtn.hidden = true;
+  clearBtn.hidden = false;
 });
 
-const palindromeResults = () => {};
+clearBtn.addEventListener('click', () => {
+  wordInput.value = '';
+  message.textContent = '';
+  clearBtn.hidden = true;
+  checkBtn.hidden = false;
+});
